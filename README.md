abbe49662b695ea82d6ec61effd83f3fe536b16f
Philippe Vignau <phi.vno@gmail.com>
2014-05-29 18:24:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# grunt-poeditor-pz
+
+> Call POEditor's APIs & download exports from a grunt task.
+
+## Getting Started
+
+First, check out [POEditor](https://poeditor.com) if you don't know what it is.
+
+This plugin requires Grunt `~0.4.5`.
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-poeditor-pz --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-poeditor-pz');
+```
+
+or using the very convenient [load-grunt-tasks](https://github.com/sindresorhus/load-grunt-tasks) utility.
+
+## Usage
+
+**This a non-tested plugin, use at your own risk.**
+On the other, it's only a convenient plugin, just take care of the downloaded exports.
+
+### Example
+
+```js
+grunt.initConfig({
+  poeditor: {
+    target1: {
+      command: { // POEditor's API args
+        action: 'list_languages',
+        id: '9999'
+      }
+    },
+    target2: {
+      download: {
+        project_id: '9999', // POEditor's project id
+        languages: { // matching POEditor's language codes with yours
+          'en-us': 'en',
+          'es': 'es',
+          'es-ar': 'es_AR',
+          'fr': 'fr'
+        },
+        type: 'po', // export type (check out the doc)
+        dest: '<%= conf.front %>/locale/?/LC_MESSAGES/django.po'
+        // grunt style dest files
+      }
+    },
+    options: {
+      api_token: '[your API token here]'
+    }
+  },
+});
+```
+
+### Options
+
+#### command
+An object specifying the API command.
+Check out the doc : https://poeditor.com/api_reference/.
+
+#### download
+Check out the doc too, at the Export command.
+Export type = po, pot, mo, xls, apple_strings, android_strings, resx, resw, properties, or json.
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+
+- 0.1.0 initial (non-tested) release
+
+## License
+MIT License, see LICENSE-MIT for details.
---
+++ tasks/poeditor.js
@@ -0,0 +1,139 @@
+/*******************************************************************************
+*
+*   Grunt Plugin : POEditor
+*   https://github.com/Philoozushi/grunt-poeditor-pz
+*
+*   Copyright (c) 2014 Philippe Vignau
+*   Licensed under the MIT license.
+*
+********************************************************************************/
+
+'use strict';
+
+var wget = require('wget'),
+	https = require('https'),
+	colors = require('colors'),
+	querystring = require('querystring');
+
+
+module.exports = function(grunt) {
+	
+	grunt.registerMultiTask('poeditor',
+	'Call POEditor\'s APIs & download exports from a grunt task',
+	function() {
+		
+		var data = this.data;
+		var opts = this.options();
+		var done = this.async();
+		
+		if (data.command) {
+			data.command.api_token = opts.api_token;
+			callAPI(data.command, function(res) {
+				
+				console.log(res);
+				done();
+			});
+		}
+		
+		else if (data.download) {
+			
+			data = data.download;
+			data.api_token = opts.api_token;
+			data.numLanguages = 0;
+			for (var polang in data.languages)
+				data.numLanguages++;
+			
+			getExports(data, function(exports) {
+				
+				for (var polang in exports)
+					grunt.log.writeln('->'.green, polang+':', exports[polang]);
+				
+				grunt.log.writeln('OK, now downloading...\n');
+				downloadExports(exports, data, function(paths) {
+					
+					for (var i in paths)
+						grunt.log.writeln('->'.red, paths[i]);
+					
+					grunt.log.writeln();
+					grunt.log.ok('All done!');
+					done();
+				});
+			});
+		}
+	});
+};
+
+function getExports(data, handler) {
+	
+	var exports = {};
+	var numLangs = data.numLanguages;
+	for (var polang in data.languages) {
+		
+		callAPI({
+			api_token: data.api_token,
+			action: 'export',
+			id: data.id,
+			language: polang,
+			type: data.type
+		},
+		function(res, command) {
+			exports[command.language] = res.item;
+			if (--numLangs == 0)
+				handler(exports);
+		});
+	}
+}
+
+function downloadExports(exports, data, handler) {
+	
+	var paths = [];
+	var numLangs = data.numLanguages;
+	for (var polang in exports) {
+		
+		var url = exports[polang];
+		var lang = data.languages[polang];
+		var path = data.dest.replace('?', lang);
+		
+		paths.push(path);
+		downloadExport(url, path, function() {
+			
+			if (--numLangs == 0)
+				handler(paths);
+		});
+	}
+}
+
+function downloadExport(url, path, handler) {
+	
+	wget.download(url, path)
+		.on('end', function(output) {
+			handler();
+		});
+}
+
+function callAPI(command, handler) {
+	
+	var postData = querystring.stringify(command);
+	
+	var req = https.request({
+		host: 'poeditor.com',
+		port: 443,
+		path: '/api/',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': postData.length
+		}
+	},
+	function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function(data) {
+			var res = JSON.parse(data);
+			handler(res, command);
+		});
+	});
+	
+	req.write(postData);
+	req.end();
+}
+
